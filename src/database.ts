82d4b677d09c9c0a258dd7e137dce5b8b074b45a
Matheus Foliatti <matheusfoliatti@gmail.com>
2023-03-10 21:42:18 -0300

    read and write database

--- src/database.ts
+++ src/database.ts
@@ -1,21 +1,37 @@
+import fs from "node:fs/promises"
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database{
 
-  database = {}
+  #persist() {
+    fs.writeFile(databasePath,  JSON.stringify(this.#database))
+  }
+
+  #database:any = {}
+
+  constructor(){
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+  }
 
   select(table:string):object {
 
-    const data = this.database[table] ?? [];
+    const data = this.#database[table] ?? [];
 
     return data
   }
 
   insert(table:string , data:object):object {
-    if (Array.isArray(this.database[table])){
+    if (Array.isArray(this.#database[table])){
       // Se sim entra aqui
-      this.database[table].push(data)
+      this.#database[table].push(data)
+      this.#persist();
     } else {
       // Se não entra aqui
-      this.database[table] = [data]
+      this.#database[table] = [data]
     }
 
     return data
