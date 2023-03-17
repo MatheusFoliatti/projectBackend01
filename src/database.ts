4e28e12496ee5723c62ebba187943d7f3ee9cde8
Matheus Foliatti <matheusfoliatti@gmail.com>
2023-03-17 20:26:49 -0300

    att

--- src/database.ts
+++ src/database.ts
@@ -1,48 +1,55 @@
-import fs from "node:fs/promises"
+import fs from "node:fs/promises";
 
-const databasePath = new URL('../db.json', import.meta.url)
+const databasePath = new URL("../db.json", import.meta.url);
 
-export class Database{
+export class Database {
+  #database: any = {};
 
-  #persist() {
-    fs.writeFile(databasePath,  JSON.stringify(this.#database, null, 2))
-  }
-
-  #database:any = {}
-
-  constructor(){
-    fs.readFile(databasePath, 'utf8')
-      .then(data => {
-        this.#database = JSON.parse(data)
-      }).catch(() => {
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
         this.#persist();
       });
   }
 
-  select(table:string):object {
-
-    const data = this.#database[table] ?? [];
-
-    return data
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2));
   }
 
-  insert(table:string , data:object):object {
-    if (Array.isArray(this.#database[table])){
+  select(table: string, id?: string): object {
+    let data = this.#database[table] ?? [];
+
+    if (id) {
+      data = data.find((row: any) => {
+        return row.id === id;
+      });
+    }
+
+    return data;
+  }
+
+  insert(table: string, data: object): object {
+    if (Array.isArray(this.#database[table])) {
       // Se sim entra aqui
-      this.#database[table].push(data)
+      this.#database[table].push(data);
       this.#persist();
     } else {
       // Se não entra aqui
-      this.#database[table] = [data]
+      this.#database[table] = [data];
     }
 
-    return data
+    return data;
   }
 
-  delete(table:string, id:string){
-    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+  delete(table: string, id: string) {
+    const rowIndex = this.#database[table].findIndex(
+      (row: any) => row.id === id
+    );
 
-    if(rowIndex > -1){
+    if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1);
       this.#persist();
     }
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,69 @@
-import { Database } from './database';
-import express from 'express';
-import { randomUUID } from 'node:crypto';
+import express from "express";
+import { randomUUID } from "node:crypto";
+import { Database } from "./database";
 
 const server = express();
 
 const port = 3333;
 
-server.use(express.json())
+server.use(express.json());
 
 const database = new Database();
 
-server.get('/', (request, response ) => {
-  const user = database.select("user")
-  response.json(user);
+const table = "user";
 
-  //response.send('Hello World! - FEMA - Alunos')
-  //response.json({msg:"Fim da Aula!!!"});
+server.get("/", (request, response) => {
+  const user = database.select(table);
+  response.json(user);
 });
 
-// Parâmetro que esta vindo do CLIENTE = REQUEST
-// Parâmetrp que esta indo para o CLIENTE = RESPONSE
+server.get("/:id", (request, response) => {
+  const { id } = request.params;
 
-server.post('/', (request, response) => {
+  const result = database.select(table, id);
+
+  // console.log(result, " - ", typeof result);
+
+  if (result === undefined)
+    response.status(400).json({ msg: "Usuário não encontrado!" });
+
+  response.json(result);
+});
+
+// Parâmetro que esta vindo do CLIENTE - REQUEST
+// Parâmetro que esta indo para o CLIENTE - RESPONSE
+
+server.post("/", (request, response) => {
   const { name, email } = request.body;
 
   const user = {
     id: randomUUID(),
-    name,
-    email
+    name: name,
+    email,
   };
 
-  database.insert('user', user)
+  database.insert(table, user);
 
-  response.status(201).json({msg:'Eu sei o que você fez no verão passado'});
+  response.status(201).json({ msg: "sucesso!" });
+});
 
-})
+server.delete("/:id", (request, response) => {
+  const { id } = request.params;
+
+  const userExist = database.select(table, id);
+
+  // console.log(result, " - ", typeof result);
+
+  if (userExist === undefined)
+    response.status(400).json({ msg: "Usuário não encontrado!" });
+
+  database.delete(table, id);
+
+  response
+    .status(202)
+    .json({ msg: `Usuário ${userExist.name} foi deletado do banco` });
+});
 
 server.listen(port, () => {
   console.log(`Server Running - end: http://localhost:${port}`);
-})
+});
